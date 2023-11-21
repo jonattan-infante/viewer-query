3feebcf5507b82a6073f5ca1ce76b4b35462486e
Jonattan Infante Vargas <jonattan@booklick.co>
2023-11-21 14:27:47 -0500

    Add migration

--- packages/api/alembic/env.py
+++ packages/api/alembic/env.py
@@ -2,12 +2,19 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
+from src.config.settings import POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+
+# Obtén la cadena de conexión de las variables de entorno
+# postgresql://user:password@localhost/dbname
+sqlalchemy_url = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres/{POSTGRES_DB}'
+
+# Actualiza la configuración con la cadena de conexión
+config.set_main_option('sqlalchemy.url', sqlalchemy_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
--- packages/api/alembic/versions/78fa3feaad86_init_migration.py
+++ packages/api/alembic/versions/78fa3feaad86_init_migration.py
@@ -1,8 +1,8 @@
-"""initial migrations
+"""Init migration
 
-Revision ID: c6a44881b5ab
+Revision ID: 78fa3feaad86
 Revises: 
-Create Date: 2023-11-11 22:22:24.359907
+Create Date: 2023-11-20 16:47:14.596336
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c6a44881b5ab'
+revision: str = '78fa3feaad86'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -24,13 +24,14 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('username', sa.String(), nullable=False),
     sa.Column('fullname', sa.String(), nullable=True),
-    sa.PrimaryKeyConstraint('id')
+    sa.PrimaryKeyConstraint('id'),
+    sa.UniqueConstraint('username')
     )
     op.create_table('queries',
     sa.Column('id', sa.Integer(), autoincrement=True, nullable=False),
     sa.Column('name', sa.String(), nullable=False),
     sa.Column('comment', sa.String(), nullable=True),
-    sa.Column('query', sa.String(), nullable=False),
+    sa.Column('query', sa.JSON(), nullable=False),
     sa.Column('user_id', sa.Integer(), nullable=True),
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
