f6fea1330cd220975954e4ba6acc96d1871a803f
Jonattan Infante Vargas <jonattan@booklick.co>
2023-11-21 16:25:31 -0500

    Fix CORS Domain Allow

--- packages/api/main.py
+++ packages/api/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","http://localhost:9000"],
     allow_methods=["*"],
     allow_headers=["*"],
     expose_headers=["*"]
